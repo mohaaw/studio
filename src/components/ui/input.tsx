20970c8a41fd43b7100752af391bf633d95fb9d6
mohammed abdulwahab <m.y.abdulwahab90@gmail.com>
2025-08-05 13:09:25 +0000

    stduy and research for the best way to make it ultra speed and responsiv

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,30 +3,20 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
-  startIcon?: React.ReactNode;
-}
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, startIcon, ...props }, ref) => {
+  ({ className, type, ...props }, ref) => {
     return (
-      <div className="relative">
-        {startIcon && (
-          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-             {startIcon}
-          </div>
+      <input
+        type={type}
+        className={cn(
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          className
         )}
-        <input
-          type={type}
-          className={cn(
-            "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-            startIcon ? "pl-10" : "",
-            className
-          )}
-          ref={ref}
-          {...props}
-        />
-      </div>
+        ref={ref}
+        {...props}
+      />
     )
   }
 )
