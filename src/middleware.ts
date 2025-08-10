f7698e832012df1f495c2e1d71ec5fa35a20a82c
mohammed abdulwahab <m.y.abdulwahab90@gmail.com>
2025-08-10 17:17:48 +0000

    Step 1: Route Protection Setup typescript// middleware.ts - Create this

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import createMiddleware from 'next-intl/middleware';
  
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['en'],
+  locales: ['en', 'ar'],
  
   // Used when no locale matches
   defaultLocale: 'en',
@@ -15,5 +15,5 @@ export default createMiddleware({
  
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(en)/:path*']
+  matcher: ['/', '/(en|ar)/:path*']
 };
